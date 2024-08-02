4843c0150acff3f39ba0a2f40217f894285d61a9
srajan <kumarsrajan02@gmail.com>
2024-08-03 02:40:17 +0530

    add: shimmer

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,6 +18,29 @@ interface Project {
 const Card: React.FC<{ project: Project }> = ({ project }) => {
   const { title, description, image, links } = project;
 
+  const shimmer = (w: number, h: number) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#202020" offset="20%" />
+        <stop stop-color="#252525" offset="50%" />
+        <stop stop-color="#202020" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#202020" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`;
+
+  const toBase64 = (str: string) =>
+    typeof window === "undefined"
+      ? Buffer.from(str).toString("base64")
+      : window.btoa(str);
+
+  interface ProjectsProps {
+    projectIdsToShow: string[];
+  }
+
   return (
     <div className="h-full border rounded-lg overflow-hidden">
       <Link href={links.live} target="_blank" className="w-full">
@@ -26,6 +49,9 @@ const Card: React.FC<{ project: Project }> = ({ project }) => {
           alt={title}
           width={500}
           height={500}
+          placeholder={`data:image/svg+xml;base64,${toBase64(
+            shimmer(500, 500)
+          )}`}
           className="object-cover hover:opacity-50 transition-all duration-200 aspect-video"
         />
       </Link>
